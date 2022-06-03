8b830129776c462cc35b659b76b6b14297fa67b4
davSizzan <ruaconazz@gmail.com>
2022-06-03 15:18:18 +0700

    gop duyet vs duong

--- Zelda-Rework-master/Zelda Project Structuring Abilities/Assets/Scripts/Enemy/DragonMover.cs
+++ Zelda-Rework-master/Zelda Project Structuring Abilities/Assets/Scripts/Enemy/DragonMover.cs
@@ -1,18 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DragonMover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    public bool mustPatrol;
+    public float circle =0;
+    public Rigidbody2D rb;
     void Start()
     {
-        
+        mustPatrol = true;
+        speed = 50;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (mustPatrol)
+        {
+            Patrol();
+        }
+        if(circle >= 2)
+        {
+            flip();
+            circle = 0;
+        }
+        circle += Time.deltaTime;
+    }
+
+    private void Patrol()
+    {
+        rb.velocity = new Vector2(speed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+    private void flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        speed *= -1;
+        mustPatrol = true;
     }
 }
