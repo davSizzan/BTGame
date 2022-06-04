d21e21a5119d46daf4a76f9402e96b563d879278
davSizzan <ruaconazz@gmail.com>
2022-06-04 21:23:11 +0700

    upproject

---
+++ Zelda-Rework-master/BTL_Game/Assets/Scripts/Reusable Components/DamageOnContact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnContact : MonoBehaviour
+{
+    [SerializeField] private string otherString;
+    [SerializeField] private float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(otherString))
+        {
+            GenericHealth temp = collision.gameObject.GetComponent<GenericHealth>();
+            if (temp)
+            {
+                temp.Damage(damage);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Zelda-Rework-master/BTL_Game/Assets/Scripts/Reusable Components/DestroyOverTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+
+{
+    [Header("Lifetime")]
+    [SerializeField] private float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
